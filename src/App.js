0ca9b59b27b8c73b46632bb738bde3ae4ff09fb8
LittleCo <little.it@outlook.com>
2020-07-06 02:08:19 +0800

    add hooks demo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 // import State from './hooksDemo/State'
 // import Effect from './hooksDemo/Effect'
-// import Context from './hooksDemo/Context'
-import StateComponent from './example/hook'
+import Ref from './hooksDemo/Ref'
+import Memo from './hooksDemo/useMemo'
+import CallBack from './hooksDemo/useCallback'
+import LayoutEffect from './hooksDemo/useLayoutEffect'
+// import StateComponent from './example/hook'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <StateComponent />
+      {/* <StateComponent /> */}
+      <Ref />
+      <hr />
+      <Memo />
+      <hr />
+      {/* <CallBack /> */}
+      <hr />
+      <LayoutEffect />
     </div>
   );
 }
