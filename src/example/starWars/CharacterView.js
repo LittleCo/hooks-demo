94ccdd68bab76526db1526f7d117651f01d0c686
LittleCo <little.it@outlook.com>
2020-07-09 01:18:42 +0800

    add characterView

--- src/example/starWars/CharacterView.js
+++ src/example/starWars/CharacterView.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import endpoint from './endpoint';
 
-const CharacterView = ({ character = {} }) => {
-  console.log(character);
-  return (
-    <section className="CharacterView">
-      <h2>{character.name}</h2>
-      <ul className="CharacterDetails">
-        <li>
-          <strong>Birth Year</strong>: {character.birthYear}
-        </li>
-        <li>
-          <strong>Eye Color</strong>: {character.eyeColor}
-        </li>
-        <li>
-          <strong>Gender</strong>: {character.gender}
-        </li>
-        <li>
-          <strong>Hair Color</strong>: {character.hairColor}
-        </li>
-        <li>
-          <strong>Heigh</strong>: {character.height}
-        </li>
-        <li>
-          <strong>Mass</strong>: {character.mass}
-        </li>
-        <li>
-          <strong>Skin Color</strong>: {character.skinColor}
-        </li>
-      </ul>
-    </section>
-  );
+const CharacterView = ({ match }) => {
+    const [character, setCharacter] = useState({})
+    console.log(character);
+
+    useEffect(() => {
+        fetch(endpoint + '/characters/' + match.params.id)
+            .then(res => res.json())
+            .then(res => setCharacter(res.character))
+    }, [match.params.id])
+
+    return (
+        <section className="CharacterView">
+            <h2>{character.name}</h2>
+            <ul className="CharacterDetails">
+                <li> 
+                    <strong>Birth Year</strong>: {character.birthYear}
+                </li>
+                <li>
+                    <strong>Eye Color</strong>: {character.eyeColor}
+                </li>
+                <li>
+                    <strong>Gender</strong>: {character.gender}
+                </li>
+                <li>
+                    <strong>Hair Color</strong>: {character.hairColor}
+                </li>
+                <li>
+                    <strong>Heigh</strong>: {character.height}
+                </li>
+                <li>
+                    <strong>Mass</strong>: {character.mass}
+                </li>
+                <li>
+                    <strong>Skin Color</strong>: {character.skinColor}
+                </li>
+            </ul>
+        </section>
+    );
 };
 
 export default CharacterView;
--- src/example/starWars/index.js
+++ src/example/starWars/index.js
@@ -1,87 +1,105 @@
 import React, { useEffect, useReducer } from 'react';
+import isFunction from 'loadsh/isFunction'
 
 import CharacterList from './CharacterList';
+import CharacterView from './CharacterView'
+
 import './styles.scss';
 import endpoint from './endpoint';
+import { Route } from 'react-router-dom';
 
 const initialState = {
-  characters: [],
-  loading: true,
-  error: null,
+    characters: [],
+    loading: true,
+    error: null,
 };
 
 const fetchReducer = (state, action) => {
-  if (action.type === 'LOADING') {
-    return {
-      characters: [],
-      loading: true,
-      error: null,
-    };
-  }
+    if (action.type === 'LOADING') {
+        return {
+            characters: [],
+            loading: true,
+            error: null,
+        };
+    }
 
-  if (action.type === 'RESPONSE_COMPLETE') {
-    return {
-      characters: action.payload.characters,
-      loading: false,
-      error: null,
-    };
-  }
+    if (action.type === 'RESPONSE_COMPLETE') {
+        return {
+            characters: action.payload.characters,
+            loading: false,
+            error: null,
+        };
+    }
 
-  if (action.type === 'ERROR') {
-    return {
-      characters: [],
-      loading: false,
-      error: action.payload.error,
-    };
-  }
+    if (action.type === 'ERROR') {
+        return {
+            characters: [],
+            loading: false,
+            error: action.payload.error,
+        };
+    }
 
-  return state;
+    return state;
 };
 
-const useFetch = url => {
-  const [state, dispatch] = useReducer(fetchReducer, initialState);
-
-  useEffect(() => {
+async function fetchCharacters(url, dispatch) {
     dispatch({ type: 'LOADING' });
-
-    const fetchUrl = async () => {
-      try {
+    try {
         const response = await fetch(url);
         const data = await response.json();
         dispatch({ type: 'RESPONSE_COMPLETE', payload: { characters: data.characters } });
-      } catch (error) {
+    } catch (error) {
         dispatch({ type: 'ERROR', payload: { error } });
-      }
-    };
+    }
+}
 
-    fetchUrl();
-  }, []);
 
-  return [state, dispatch];
-};
+const useThunkReducer = (reducer, initialState) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    useEffect(() => {
+        fetchCharacters(endpoint + '/characters', dispatch)
+    }, [])
+
+    const enhancedDispatch = React.useCallback(action => {
+        console.log(action);
+
+        if (isFunction(action)) {
+            action(dispatch)
+        } else {
+            dispatch(action)
+        }
+
+    }, [dispatch])
+
+    return [state, enhancedDispatch]
+}
 
 const Application = () => {
-  const [state, dispatch] = useFetch(endpoint + '/characters');
-  console.log(state)
-  const { characters, loading, error } = state;
+    const [state, dispatch] = useThunkReducer(fetchReducer, initialState);
+    console.log(state)
+    const { characters, loading, error } = state;
 
-  return (
-    <div className="Application">
-      <header>
-        <h1>Star Wars Characters</h1>
-      </header>
-      <main>
-        <section className="sidebar">
-          {loading ? (
-            <p>Loading…</p>
-          ) : (
-            <CharacterList characters={characters} />
-          )}
-          {error && <p className="error">{error.message}</p>}
-        </section>
-      </main>
-    </div>
-  );
+    return (
+        <div className="Application">
+            <header>
+                <h1>Star Wars Characters</h1>
+            </header>
+            <main>
+                <section className="sidebar">
+                    {loading ? (
+                        <p>Loading…</p>
+                    ) : (
+                            <CharacterList characters={characters} />
+                        )}
+                    {error && <p className="error">{error.message}</p>}
+                </section>
+                <section className='CharacterView'>
+                    <Route path='/characters/:id' component={CharacterView} />
+                </section>
+            </main>
+        </div>
+    );
 };
 
 export default Application;
